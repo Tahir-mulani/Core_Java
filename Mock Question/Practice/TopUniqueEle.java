8243bee67445d770d047db6f5c9e43af9be76e09
Tahir-mulani <tahirmulani7447@gmail.com>
2025-11-12 23:14:03 +0530

    array practice

--- Mock Question/Practice/TopUniqueEle.java
+++ Mock Question/Practice/TopUniqueEle.java
@@ -5,46 +5,55 @@ Q8. Write a java program to perform this operation.
 Sample Input: [78, 90, 90, 86, 85, 92, 70, 92]
 Expected Output: 92, 90, 86
 */
-
+import java.util.Scanner;	
 public class TopUniqueEle
 {
 	public static void main(String x[])
 	{
-		//Scanner sc = new Scanner(System.in);
-		int a[]={78,90,90,86,85,92,70,92};
-		/*
-		System.out.printf("enter array element");
-		for(int i=0;i<a.length;i++)
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter array length");
+		int n=sc.nextInt();
+		int arr[]=new int[8];
+		System.out.println("enter array element");
+		for(int i=0;i<arr.length;i++)
 		{
-			a[i]=sc.nextInt();
+			arr[i]=sc.nextInt();
 		}
-		*/
 		
-		for(int i=0;i<a.length;i++)
+		//array sort in descending order.
+		for(int i=0;i<arr.length;i++)
 		{
-			for(int j=i+1;j<a.length;j++)
+			for(int j=i+1;j<arr.length;j++)
 			{
-				if(a[i]>a[j])
+				if(arr[i]<arr[j])
 				{
-					int temp=a[i];
-					a[i]=a[j];
-					a[j]=temp;
+					int temp=arr[j];
+					arr[j]=arr[i];
+					arr[i]=temp;
 				}
 			}
 		}
 		
-		
-		int right=0,left=1;
-		
-		while(a[right]<a.length && a[left]<a.length)
+		System.out.println("array Descending order");
+		for(int a:arr)
 		{
-			if(a[right]==a[left])
-			{
-				System.out.printf("%d\t",a[right]);
-				right++;
-				left++;
-			}
-			left++;
+			System.out.print(a+" ");
 		}
+		
+		System.out.println("top 3 highest unique scores.");
+		//find three highest scores
+		int count=0;
+		int last=0;
+		for(int i=0;i<arr.length&& count<3;i++)
+		{
+			if(arr[i]!=last)
+			{
+				System.out.print(arr[i]+" ");
+				last=arr[i];
+				count++;
+			}
+		}
+		
+		
 	}
 }
